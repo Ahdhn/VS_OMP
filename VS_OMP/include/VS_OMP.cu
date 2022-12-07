64986f220deed4a9a13a47ebd53c4c77fdaf0098
Ahmed Mahmoud <ahmed.mahmoud@autodesk.com>
2022-12-06 19:20:37 -0500

    test code

--- VS_OMP/include/VS_OMP.cu
+++ VS_OMP/include/VS_OMP.cu
@@ -1,7 +1,15 @@
 #include <omp.h>
+
+#include <assert.h>
+#include <cuda_runtime.h>
 #include <stdio.h>
 
 #define MAX 10
+
+__global__ void kernel(int tid)
+{
+    printf("\n I am omp thread %d\n", tid);
+}
 
 int main()
 {
@@ -10,6 +18,8 @@ int main()
     {
 #pragma omp atomic
         count++;
+        kernel<<<1, 1>>>(omp_get_thread_num());
     }
+    cudaDeviceSynchronize();
     printf_s("Number of threads: %d\n", count);
 }
